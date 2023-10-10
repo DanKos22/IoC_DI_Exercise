a17975c39bb1cdb0247c575579588a3654f31409
DanKos22 <G00397054@atu.ie>
2023-10-10 15:26:22 +0100

    Starting project

---
+++ src/main/java/ie/atu/ioc_di_exercise/IoCDiExerciseApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.ioc_di_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCDiExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoCDiExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/ioc_di_exercise/IoCDiExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.ioc_di_exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoCDiExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
