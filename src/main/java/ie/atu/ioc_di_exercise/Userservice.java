98a7934439e4d6a8dc95023b38cd0e8ff064654c
DanKos22 <G00397054@atu.ie>
2023-10-13 15:51:19 +0100

    Second exercise done

--- src/main/java/ie/atu/ioc_di_exercise/Userservice.java
+++ src/main/java/ie/atu/ioc_di_exercise/Userservice.java
@@ -1,8 +1,10 @@
 package ie.atu.ioc_di_exercise;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Data
 @Service
 public class Userservice {
 
@@ -14,9 +16,17 @@ public class Userservice {
         this.emailservice = emailservice;
     }
 
+    // Method for 1st exercise
     public String registerUser(String name, String email)
     {
-        String message = emailservice.sendEmail(email, "Welcome to our platform\t" + name + "\t" + email);
+        String message = emailservice.sendEmail(email,"Welcome to our platform\t" + name + "\t" + email);
+        return message;
+    }
+
+    // Method for confirmAndRegister/Part 2 exercise
+    public String createMessage(UserDetails user)
+    {
+        String message = "Welcome to our platform\t" + user.getName() + "\t" + user.getEmail();
         return message;
     }
 }
