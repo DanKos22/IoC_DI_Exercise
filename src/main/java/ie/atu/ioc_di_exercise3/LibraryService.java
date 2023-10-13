eca7480cdf902a7659207606119aeb83b02b046a
DanKos22 <G00397054@atu.ie>
2023-10-14 00:00:02 +0300

    Added some code

--- src/main/java/ie/atu/ioc_di_exercise3/LibraryService.java
+++ src/main/java/ie/atu/ioc_di_exercise3/LibraryService.java
@@ -1,27 +1,43 @@
 package ie.atu.ioc_di_exercise3;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Data
+@Service
 public class LibraryService {
 
     private Book book;
+    ArrayList<String> myList = new ArrayList<String>();
 
     @Autowired
-    public void setBook(Book book)
-    {
-        this.book = book;
-    }
 
     public String addBook(Book book)
     {
-        return book.getConfirmation();
+        String object = "Title:\t" + book.getTitle() + "Author:\t" + book.getAuthor() +"Isbn\t" + book.getIsbn() + "Published in:\t" + book.getPublishedYear();
+        myList.add(object);
+        return object;
     }
 
     public String getBooks()
     {
-        return book.getList();
+        String list = loop(myList);
+        return list;
     }
 
+    public static String loop(ArrayList<String> someArrayList)
+    {
+        int indexvalue = 0;
+
+        for(String value: someArrayList)
+        {
+            System.out.println("Book at index " + indexvalue + ":" + value);
+            indexvalue++;
+        }
+    }
 }
